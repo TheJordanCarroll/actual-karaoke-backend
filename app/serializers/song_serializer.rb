b6149a2746a7bc2fe134ffed02be6cc909be7604
Jordan Carroll <jordancarroll1994@gmail.com>
2021-03-22 21:42:32 -0400

    got the create favorite song and delete favorite song working

--- app/serializers/song_serializer.rb
+++ app/serializers/song_serializer.rb
@@ -1,7 +1,7 @@
 class SongSerializer < ActiveModel::Serializer
-    attributes :id, :name, :artist_id, :artist_gender, :lowest_pitch, :highest_pitch, :lowest_note, :highest_note, :genre_id, :year, :tempo, :mood, :key, :image, :rap_sing, :range, :hit_year
-    has_one :genre
-    has_one :artist
-    has_many :favorite_songs
-    has_many :users, through: :favorite_songs
+    attributes :id, :name, :artist, :artist_gender, :lowest_pitch, :highest_pitch, :lowest_note, :highest_note, :genre, :year, :tempo, :mood, :key, :image, :rap_sing, :range, :hit_year
+    # has_one :genre
+    # has_one :artist
+    # has_many :favorite_songs
+    # has_many :users, through: :favorite_songs
 end
